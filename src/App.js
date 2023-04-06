eac9e55d857eddfa7626e6bc03e573d2fad42631
Tony Mugendi <tony.mugendi@twigafoods.com>
2023-04-06 23:44:56 +0300

    Add docker tag

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello Tony
+          Hello New Deployment
         </a>
       </header>
     </div>
